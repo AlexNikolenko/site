f2790bf83a3580a86c1d5e7da2a426f2103208a5
AlexNikolenko <144997089+AlexNikolenko@users.noreply.github.com>
2023-09-15 00:14:49 +0300

    Update particle-text.js

--- particle-text.js
+++ particle-text.js
@@ -43,7 +43,7 @@ function Particle(x, y) {
   this.y = y;
   this.vx = 0;
   this.vy = 0;
-  this.r = 4;
+  this.r = 2;
   this.theta = (Math.random() * 2 - 1)*0.02;
   this.thetab = 8;
   this.c = "rgba(250, 250, 255, 1.0)";
