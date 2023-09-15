bb7861ce7c247c0c9c2921bc0e598b464b2e56db
AlexNikolenko <144997089+AlexNikolenko@users.noreply.github.com>
2023-09-15 15:55:30 +0300

    Update particle-text.js

--- particle-text.js
+++ particle-text.js
@@ -19,7 +19,7 @@ var tch = textCanvas.height = ch;
 var mouseX = 0, mouseY = 0;
 
 var textStr = "ВИКА+ЛЁША";
-var num = 1200;
+var num = 1000;
 var particles = [];
 
 var toff = 0.0;
@@ -53,14 +53,14 @@ function Particle(x, y) {
   this.y = y;
   this.vx = 0;
   this.vy = 0;
-  this.r = 3;
-  //this.r = 2;
+  //this.r = 3;
+  this.r = 2.5;        // диаметр точки
   this.theta = (Math.random() * 2 - 1)*0.02;
   // this.thetab = 5;
-  this.thetab = 3;
-  this.c = "rgba(250, 250, 255, 1.0)";
+  this.thetab = 3;      // расстояние перемещения
+  this.c = "rgba(250, 250, 255, 0.8)";
   // this.lineLength = 250;
-  this.lineLength = 100;
+  this.lineLength = 120;    // максимально возможная длина линии связи между точками
 }
 
 Particle.prototype.draw = function() {
